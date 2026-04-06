9c77d928ef7c2d1783a049bb4a2fa2b4654363ff
CrayZan <albarracincristian470@gmail.com>
2026-04-06 01:49:48 -0300

    Update vite.config.ts

--- Proyecto final RS/FRONTED/vite.config.ts
+++ Proyecto final RS/FRONTED/vite.config.ts
@@ -2,17 +2,11 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Esto permite que el código encuentre tus componentes usando "@"
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    // Asegura que la carpeta de salida sea la que espera Vercel
-    outDir: 'dist',
-  },
-});
+})
