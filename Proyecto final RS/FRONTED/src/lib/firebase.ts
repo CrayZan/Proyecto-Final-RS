e1a5719c08735c5a22795bc3015915576e7ecd0d
CrayZan <albarracincristian470@gmail.com>
2026-04-06 05:09:51 -0300

    Update firebase.ts

--- Proyecto final RS/FRONTED/src/lib/firebase.ts
+++ Proyecto final RS/FRONTED/src/lib/firebase.ts
@@ -1,10 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// Esta es tu configuración real extraída de la imagen que enviaste
 const firebaseConfig = {
   apiKey: "AIzaSyCocpq3xsGl0pJ8eZhr-PFvFktc9jUtbjU",
   authDomain: "resto-bar-628e4.firebaseapp.com",
+  // ESTA LÍNEA ES CLAVE: Es la dirección de tu base de datos
+  databaseURL: "https://resto-bar-628e4-default-rtdb.firebaseio.com", 
   projectId: "resto-bar-628e4",
   storageBucket: "resto-bar-628e4.firebasestorage.app",
   messagingSenderId: "47430218533",
@@ -12,8 +13,5 @@ const firebaseConfig = {
   measurementId: "G-X2V1GREN2R"
 };
 
-// Inicializamos la App
 const app = initializeApp(firebaseConfig);
-
-// Exportamos la base de datos para usarla en el Menú y las Comandas
 export const db = getDatabase(app);
